3d4e5215f050a5fa93f11329a8a9b22eee3320a8
tanabetetsuya <tetsumagic123@gmail.com>
2021-05-12 04:33:35 +0000

    [Finish] 3-8

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_05_08_134017) do
 
   create_table "books", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "body", null: false
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
